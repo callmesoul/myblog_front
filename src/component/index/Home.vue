9e358f1322efe2eaec21bfa16bfb78db0a7696cd
callmesoul <547437716@qq.com>
2016-12-30 08:36:55 +0800

    init

---
+++ src/component/index/Home.vue
@@ -0,0 +1,252 @@
+<template>
+    <div class="warp-right">
+        <div class="container-pathnav">
+            <a>首页</a>
+            <a class="active">前端开发</a>
+        </div>
+        <div class="container-content">
+            <div class="container-content-container">
+                <swiper :options="swiperOption">
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont icon-liulan-copy"></i>56</span>
+                                    <span><i class="iconfont icon-pinglun"></i>56</span>
+                                    <span><i class="iconfont icon-shijian"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <a class="article-item">
+                            <div class="image">
+                                <img src="src/assets/images/article.jpg">
+                            </div>
+                            <div class="content">
+                                <div class="title">开博啦！开博啦！踏上大神的第一</div>
+                                <div class="drsc">
+                                    本博客主要用Duxcms搭建用于记录博主的点点滴滴，无论是生活上的还是技术上的。博客刚建好，还不完善请见谅。后面会慢慢完善跟扩展。敬请期待
+                                </div>
+                                <div class="msg">
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                    <span><i class="iconfont"></i>56</span>
+                                </div>
+                            </div>
+                        </a>
+                    </swiper-slide>
+                    <div class="swiper-scrollbar" slot="scrollbar"></div>
+                </swiper>
+            </div>
+        </div>
+    </div>
+
+</template>
+<style>
+
+</style>
+<script>
+    export default {
+    data() {
+      return {
+        swiperOption: {
+          scrollbar: '.swiper-scrollbar',
+          direction: 'vertical',
+          slidesPerView: '2',
+          slidesPerColumn : 3,
+          scrollbarHide:false,
+          mousewheelControl: true,
+          freeMode: true
+        }
+      }
+    }
+  }
+</script>
